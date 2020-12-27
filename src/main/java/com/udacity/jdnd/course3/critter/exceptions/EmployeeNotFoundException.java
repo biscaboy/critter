046a41093d1b479f1b44efd632a398dc95ee9849
biscaboy <david@davidjdickinson.com>
2020-12-27 15:35:12 -0700

    feat: remove reason messages errors
    Removed the messages from @ResponseStatus statements.

--- src/main/java/com/udacity/jdnd/course3/critter/exceptions/EmployeeNotFoundException.java
+++ src/main/java/com/udacity/jdnd/course3/critter/exceptions/EmployeeNotFoundException.java
@@ -3,7 +3,7 @@ package com.udacity.jdnd.course3.critter.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Employee not found.")
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class EmployeeNotFoundException extends RuntimeException {
 
     public EmployeeNotFoundException() {
--- src/main/java/com/udacity/jdnd/course3/critter/exceptions/MissingParameterException.java
+++ src/main/java/com/udacity/jdnd/course3/critter/exceptions/MissingParameterException.java
@@ -3,7 +3,8 @@ package com.udacity.jdnd.course3.critter.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Employee skills were missing from the request.")
+//@ResponseStatus(code = HttpStatus.BAD_REQUEST, reason = "Required information was missing from the request.")
+@ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class MissingParameterException extends RuntimeException {
 
     public MissingParameterException() {
